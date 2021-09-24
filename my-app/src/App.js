c102d7398086aca99bcef094fba30b5528c97b9a
samSOUMAH <sam86soumah@gmail.com>
2021-09-25 01:31:38 +0200

    changement

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,14 +1,11 @@
 import ListElem from "./Components/ListElem";
-import FilterList from "./Components/FilterList"
-import AddList from "./Components/AddList"
+
 
 
 function App() {
   return (
     <div className="App">
-      <AddList></AddList>
-      <hr />
-      <FilterList ></FilterList>
+    
       <ListElem></ListElem>
       
     </div>
--- my-app/src/Components/ListElem.js
+++ my-app/src/Components/ListElem.js
@@ -1,64 +1,65 @@
+
 import {Button} from 'react-bootstrap'
 import "bootstrap/dist/css/bootstrap.min.css"
-import { faTrash } from '@fortawesome/free-solid-svg-icons'
+import { faPenFancy, faTrash } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import React, { Component } from 'react'
-import List from './List'
+import FilterList from './FilterList';
+import AddList from './AddList'
+import React,{Component} from 'react'
 
-export default class ListElem extends Component {
-
+ export default class ListElem extends Component {
     state={
+        data:[
 
-     data:[
             { id: 1,
-             name:'Task number 1',
-             bouton:<Button variant="danger"><FontAwesomeIcon icon={faTrash}></FontAwesomeIcon></Button>
-            },
+              name:'Task number 1'
+           
+             },
+  
+             { id: 2,
+               name:'Task number 2'
+                
+          
+               
+             },
+  
+             { id:3,
+               name:'Task number 3'
+            
+             }
+  
+       ]
 
-            { id: 2,
-              name:'Task number 2',
-              bouton:<Button variant="danger"><FontAwesomeIcon icon={faTrash}></FontAwesomeIcon></Button>
-            },
-
-            { id:3,
-              name:'Task number 3',
-             bouton:<Button variant="danger"><FontAwesomeIcon icon={faTrash}></FontAwesomeIcon></Button>
-            }
-          ]
     }
 
+    
+   render(){
+    return (
+        <div>
+           <div className="text-center m-1">
+             <h1>ADD_LIST</h1>
+           <AddList/>
+           </div>
+         
+             <div>
+             <FilterList ></FilterList>
+             </div>
 
-    render() {
-        return (
-            <div>
-                {this.state.data.map((data) => 
-                (
-                    <div className="List container border bg-warning "> 
-                    <ul className="list-group d-flex flex-row justify-content-between">
+            {this.state.data.map((data) => 
+            (
+                <div className="List d-flex container justify-content-between border bg-warning m-2">      
+               <h5> {data.name}</h5>  
+                <Button variant="danger" className=""><FontAwesomeIcon icon={faTrash}></FontAwesomeIcon></Button>      
+                </div>
+                )
+           )}
+         
+        </div>
+ 
+    )
+} 
 
-                        <li className="list-group-item bg-warning">
-                        {data.id} 
-                        </li>
-                        <li className="list-group-item bg-warning">
-                         <h5> {data.name}</h5>
-                        </li>
-                        <li className="list-group-item float-end bg-warning">
-                        {data.bouton}
-                        </li>
 
-                    </ul>
-                       
-                       
-                       
-                       
-                    </div>
-                    )
-               )}
-             
-            </div>
-     
-        )
-    } 
 }
-
+     
 
