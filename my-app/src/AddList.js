e7d7ed3724db2d02cecdd81ff69e8e4fb61cf798
samSOUMAH <sam86soumah@gmail.com>
2021-09-25 20:35:39 +0200

    Add_List

---
+++ my-app/src/AddList.js
@@ -0,0 +1,35 @@
+import {useRef} from 'react'
+import {Button} from 'react-bootstrap'
+import "bootstrap/dist/css/bootstrap.min.css"
+import { faPenFancy, faTrash, faSearch, faPlusSquare} from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+
+
+const AddList = (props) => {
+ const inputTask = useRef("");
+
+ const onAddTask= () => {
+     let input_value=inputTask.current.value;
+     inputTask.current.value="";
+    // console.log(input_value);
+     props.addTaskList(input_value)
+    
+ }
+
+    return (
+        <div>
+             <div className="d-flex justify-content-center m-2"> 
+
+              <input type="text" 
+              placeholder="Add todo"
+              className="form-control w-50 bg-warning"
+              ref={inputTask} />
+              <Button variant="success" onClick={()=>{ onAddTask()}}><FontAwesomeIcon icon={faPlusSquare} ></FontAwesomeIcon></Button>
+              </div>
+
+    
+        </div>
+    )
+}
+
+export default AddList;
--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,13 +1,46 @@
-import ListElem from "./Components/ListElem";
+import {useState, useRef} from 'react'
+import AddList from './AddList'
+import {Button} from 'react-bootstrap'
+import "bootstrap/dist/css/bootstrap.min.css"
+import { faPenFancy, faTrash, faSearch, faPlusSquare} from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+
+
+
 
 
 
 function App() {
+ 
+  const [data, setData]=useState(["Bonjour","Midi", "Soirée", "Nuit"]);
+  const AddNewTaskList=(taskNew)=>{
+  
+    if(taskNew!==""){setData([...data, taskNew ])
+    alert('bien ajouter')}
+    else{alert('Vous n\'avez pas de valeur saisi')}
+  }
+
+
   return (
     <div className="App">
-    
-      <ListElem></ListElem>
-      
+      <h1 className="text-center">ADD_LIST</h1>
+    <div>
+
+    <AddList addTaskList={AddNewTaskList}/>
+
+    </div>
+
+    <div className="d-flex justify-content-center">
+      <input type="text" placeholder="Seach..." className="form-control w-50 bg-warning"></input><FontAwesomeIcon icon={faSearch} style={{with:'3rem', height:'3rem'}} className="border bg-warning"></FontAwesomeIcon>
+    </div>
+
+      {data.map((user)=>
+      <div className="container  d-flex justify-content-between border m-2 bg-warning ">
+          <h4>{user}</h4>
+          <Button variant="danger" ><FontAwesomeIcon icon={faTrash}></FontAwesomeIcon></Button>
+      </div>
+      )}
+     
     </div>
   );
 }
